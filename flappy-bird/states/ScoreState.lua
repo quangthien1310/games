36660be9f4abd732f721ab2dd05354708f63df5d
Winter-0101 <quangthien1310@gmail.com>
2024-05-16 11:51:36 +0700

    update medal feature

--- flappy-bird/states/ScoreState.lua
+++ flappy-bird/states/ScoreState.lua
@@ -1,5 +1,9 @@
 ScoreState = Class{__includes = BaseState}
 
+local bronzeMedal = love.graphics.newImage('bronzemedal.png')
+local silverMedal = love.graphics.newImage('silvermedal.png')
+local goldMedal = love.graphics.newImage('goldmedal.png')
+
 function ScoreState:enter(params)
     self.score = params.score
 end
@@ -17,4 +21,12 @@ function ScoreState:render()
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
     love.graphics.printf('Press Enter to play again', 0, 160, VIRTUAL_WIDTH, 'center')
+
+    if self.score >= 6 then
+        love.graphics.draw(goldMedal, 10, 10, 0, .25, .25)
+    elseif self.score >= 4 then
+        love.graphics.draw(silverMedal, 10, 10, 0, .25, .25)
+    elseif self.score >= 2 then
+        love.graphics.draw(bronzeMedal, 10, 10, 0, .25, .25)
+    end
 end

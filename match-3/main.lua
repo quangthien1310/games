12ce1317d992b6dba34c1bf4f0c689940386a248
Winter-0101 <quangthien1310@gmail.com>
2024-06-08 14:40:17 +0700

    state update

--- match-3/main.lua
+++ match-3/main.lua
@@ -1,11 +1,76 @@
-push = require 'push'
+love.graphics.setDefaultFilter('nearest', 'nearest')
 
-VIRTUAL_WIDTH = 384
-VIRTUAL_HEIGHT = 216
+require 'src/Dependencies'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+BACKGROUND_SCROLL_SPEED = 80
+
 function love.load()
+
+    love.window.setTitle('Match 3')
+
+    math.randomseed(os.time())
     
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true,
+        canvas = true
+    })
+
+    gSounds:['music']:setLooping(true)
+    gSounds:['music']:play()
+
+    gStateMachine = StateMachine {
+        ['start'] = function() return StartState() end,
+        ['begin-game'] = function() return BeginGameState() end,
+        ['play'] = function() return PlayState() end,
+        ['game-over'] = function() return GameOverState() end
+    }
+    gStateMachine:change('start')
+
+    backgroundX = 0
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+function love.wasPressed(key)
+    if love.keyboard.leysPressed[key] = true then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    backgroundX = backgroundX - BACKGROUND_SCROLL_SPEED * dt
+    if backgroundX <= -1024 + VIRTUAL_WIDTH - 4 + 51 then
+        backgroundX = 0 
+    end
+
+    gStateMachine:update(dt)
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+    
+    love.graphics.draw(gTextures['background'], backgroundX, 0)
+    gStateMachine:render()
+
+    push:finish()
 end
